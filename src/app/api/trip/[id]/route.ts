e3d1bc816afbc6fe96a6b2ea27c2730bc612952e
KaoM3 <rafaeldcbernandino@gmail.com>
2025-07-16 15:33:40 +0800

    feat(trip): add route for crud

---
+++ src/app/api/trip/[id]/route.ts
@@ -0,0 +1,38 @@
+import { getTrip, deleteTrip } from "@features/trip/services/crud";
+import { validateIdParam } from "@/lib/utils";
+
+/**
+ * GET /api/trip/[id]
+ *
+ * Gets a trip's information based on the dynamic `id` parameter in the URL path.
+ * Example request: GET /api/trip/123
+ * 
+ * Route param:
+ * - id (string): trip ID passed as part of the URL (e.g., /api/trip/123)
+ */
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+  const id = validateIdParam(params.id);
+  if (id instanceof Response) {
+    return id;
+  } else {
+    return getTrip(id);
+  }
+}
+
+/**
+ * DELETE /api/trip/[id]
+ *
+ * Deletes a trip based on the dynamic `id` parameter in the URL path.
+ * Example request: DELETE /api/trip/123
+ * 
+ * Route param:
+ * - id (string): trip ID passed as part of the URL (e.g., /api/trip/123)
+ */
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  const id = validateIdParam(params.id);
+  if (id instanceof Response) {
+    return id;
+  } else {
+    return deleteTrip(id);
+  }
+}
