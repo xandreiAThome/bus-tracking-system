7df6524c4c9326ec6504571a6e11c25da7d7cb7a
flatlayn <raistephen.isidro@gmail.com>
2025-07-17 03:58:48 +0800

    Complete layout, no buttons

--- src/app/issuedTickets/page.tsx
+++ src/app/issuedTickets/page.tsx
@@ -2,11 +2,13 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from '@/components/ui/button';
-import TripCard from '@/features/trips/components/tripCard';
+import TicketCard from '@/features/tickets/components/ticketCard';
 
-const dummyTrips = [
-  {route: 'ALLEN → CATARMAN', time: '9:00 PM', driver: 'Juan Dela Cruz'},
-  {route: 'MANILA → DAVAO', time: '9:00 PM', driver: 'Rage Del Fiero'}
+const dummyTickets = [
+  {seat:4, price: 115, passenger: 'Maria Leonora Theresa', ticketNo: '#3901', dateTime: 'JUL 24 2025 12:25 PM'},
+  {seat:5, price: 115, passenger: 'Astherielle Rafael', ticketNo: '#3902', dateTime: 'JUL 24 2025 12:25 PM'},
+  {seat:6, price: 115, passenger: 'Kidlat Adlawan', ticketNo: '#3903', dateTime: 'JUL 24 2025 12:25 PM'},
+  {seat:7, price: 115, passenger: 'Kidlat Adlawan', ticketNo: '#3904', dateTime: 'JUL 24 2025 12:25 PM'},
 ]
 
 export default function TripsOverview() {
@@ -17,25 +19,27 @@ export default function TripsOverview() {
           <div className='flex flex-col items-center'>
             <div className='w-16 h-1.5 rounded-full bg-[#90A38B] mb-2'></div>
             <CardTitle className='mt-2 font-extrabold text-[#456A3B]'>
-              Trips Overview
+              Issued Tickets
             </CardTitle>
           </div>
         </CardHeader>
         <CardContent className='flex-1 overflow-y-auto mt-4'>
-          <div className='flex flex-col gap-y-4'>
-            {dummyTrips.map((trip, index) => (
-              <TripCard
-                key={index}
-                route={trip.route}
-                time={trip.time}
-                driver={trip.driver}
+          <div className='flex flex-col gap-y-2'>
+            {dummyTickets.map((ticket, index) => (
+              <TicketCard
+                key = {index}
+                seat={ticket.seat}
+                price={ticket.price}
+                passenger={ticket.passenger}
+                ticketNo={ticket.ticketNo}
+                dateTime={ticket.dateTime}
               />
-            ))}
+              ))}
           </div>
         </CardContent>
         <div className='flex mt-4 justify-center'>
           <Button className='h-max w-[80%] bg-[#71AC61] hover:bg-[#456A3B] font-bold text-xl rounded-lg'>
-            Create Trip
+            Issue Ticket
           </Button>
         </div>
       </Card>
