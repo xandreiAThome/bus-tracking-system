da12d9bff6776d4e9da5f23c6b045ca6d46883bf
KaoM3 <rafaeldcbernandino@gmail.com>
2025-07-16 15:52:13 +0800

    feat(ticket): add api routes

---
+++ src/app/api/ticket/route.ts
@@ -0,0 +1,38 @@
+import { getTicket, deleteTicket } from "@features/ticket/services/crud";
+import { validateIdParam } from "@/lib/utils";
+
+/**
+ * GET /api/ticket/[id]
+ *
+ * Gets a ticket's information based on the dynamic `id` parameter in the URL path.
+ * Example request: GET /api/ticket/123
+ * 
+ * Route param:
+ * - id (string): ticket ID passed as part of the URL (e.g., /api/ticket/123)
+ */
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+  const id = validateIdParam(params.id);
+  if (id instanceof Response) {
+    return id;
+  } else {
+    return getTicket(id);
+  }
+}
+
+/**
+ * DELETE /api/ticket/[id]
+ *
+ * Deletes a ticket based on the dynamic `id` parameter in the URL path.
+ * Example request: DELETE /api/ticket/123
+ * 
+ * Route param:
+ * - id (string): ticket ID passed as part of the URL (e.g., /api/ticket/123)
+ */
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  const id = validateIdParam(params.id);
+  if (id instanceof Response) {
+    return id;
+  } else {
+    return deleteTicket(id);
+  }
+}
