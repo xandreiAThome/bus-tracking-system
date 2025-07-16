49132e2eeeae669c53a3610fd65cc31e0a44089e
KaoM3 <rafaeldcbernandino@gmail.com>
2025-07-16 17:16:34 +0800

    feat(bus): add api route for getting all seats of specific bus

---
+++ src/app/api/bus/[id]/seats/route.ts
@@ -0,0 +1,29 @@
+import { getSeatsByBus } from "@features/seat/services/crud";
+import { validateIdParam } from "@/lib/utils";
+import { validateSortOrder } from "@/lib/utils";
+
+/**
+ * GET /api/bus/[id]/seats
+ *
+ * Gets the seats associated with a bus based on the dynamic `id` parameter.
+ * Example request: GET /api/bus/123/seats
+ * 
+ * Route param:
+ * - id (string): bus ID passed as part of the URL (e.g., /api/bus/123/seats)
+ * 
+ * Optional param:
+ * - sortOrder (string): sort order, either `ASC` or `DESC`
+ */
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+  const id = validateIdParam(params.id);
+  const url = new URL(req.url);
+  const rawSortOrder = url.searchParams.get('sortOrder');
+
+  const validatedSortOrder = validateSortOrder(rawSortOrder)
+
+  if (id instanceof Response) {
+    return id;
+  } else {
+    return getSeatsByBus(id, validatedSortOrder);
+  }
+}
