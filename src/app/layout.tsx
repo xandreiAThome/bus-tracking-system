545de565496e95ff4da14c36f8ab7531f11a98bd
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-07-20 17:38:33 +0800

    feat: implemented map and auto tracking
    TODO: connect it to the busId from the tripScreen after backend
    integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AlignJustify } from "lucide-react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full relative`}
       >
-        <header className="p-2 absolute">
-          <AlignJustify className="text-green-800" />
-        </header>
         {children}
       </body>
     </html>
---
+++ src/components/navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Undo2, AlignJustify, House } from "lucide-react";
+import { SheetTrigger, SheetContent, Sheet, SheetTitle } from "./ui/sheet";
+import { usePathname, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export default function NavBar() {
+  const pathname = usePathname();
+  const router = useRouter();
+  return (
+    <header
+      className={cn(
+        "p-2 bg-black/10 sm:bg-black/0 absolute flex w-full",
+        pathname !== "/tripsOverview" ? "justify-between" : "justify-end"
+      )}
+    >
+      {pathname !== "/tripsOverview" && (
+        <Undo2 className="text-green-700" onClick={() => router.back()} />
+      )}
+
+      <Sheet>
+        <SheetTrigger>
+          <AlignJustify className="text-green-700" />
+        </SheetTrigger>
+        <SheetContent className="px-8 py-10 text-lg text-green-800 font-bold">
+          <SheetTitle className="text-green-800">Navigation Menu</SheetTitle>
+          <Link href={"/tripsOverview"} className="flex items-center gap-2">
+            <House />
+            Home
+          </Link>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
+}
