c3222a4312756577e09c73c723d355f664c59208
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-07-24 16:55:46 +0800

    refactor: moved login to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import SignInForm from "@/features/auth/components/signInForm";
 import { auth } from "@/features/auth/services/auth";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
-export default async function Home() {
+export default async function Login() {
   const session = await auth();
   if (session?.user) {
     redirect("/tripsOverview");
   }
   return (
-    <div className="w-full h-full flex items-center justify-center flex-col relative">
-      <h1 className="text-5xl font-bold text-green-700 top-10 absolute">
-        Bus Tracking System
-      </h1>
-      <SignInForm></SignInForm>
+    <div className="min-h-full flex items-center justify-center bg-gray-50">
+      <Card className="w-10/12 sm:w-[350px] md:w-[450px] lg:w-[600px] shadow-lg">
+        <CardHeader className="flex flex-col items-center pt-6 justify-center">
+          <Image
+            src="/amoec.jpg"
+            alt="logo"
+            width={150}
+            height={150}
+            className="w-24 h-24 sm:w-32 sm:h-32 object-contain"
+          ></Image>
+          <CardTitle className="text-3xl font-bold mt-1 mb-[-10] text-center">
+            Trip Management System
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="flex justify-center pb-6">
+          <SignInForm></SignInForm>
+        </CardContent>
+      </Card>
     </div>
   );
 }
