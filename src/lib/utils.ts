97b71b5937c9c625db6ef8ee735cfb125502ffbb
KaoM3 <rafaeldcbernandino@gmail.com>
2025-07-29 20:26:30 +0800

    feat(utils): add util function for parsing prisma errors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-
+import { PrismaClient, Prisma } from '@/generated/prisma'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -217,4 +217,26 @@ export function generateSeatNumbers(capacity: number): string[] {
     seats.push(`S${i.toString().padStart(2, "0")}`); // Formats as S01, S02, etc.
   }
   return seats;
+}
+
+export function parsePrismaError(err: unknown): {
+  status: number;
+  message: string;
+} {
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    switch (err.code) {
+      case "p2000":
+        return { status: 409, message: "Invalid input"};
+      case "P2002":
+        return { status: 409, message: "Duplicate entry" };
+      case "P2001":
+      case "P2025":
+        return { status: 404, message: "Record not found" };
+      case "P2003":
+        return { status: 400, message: "Invalid foreign key reference" };
+      case "2011":
+        return { status: 400, message: "Missing required fields" };
+    }
+  }
+  return { status: 500, message: 'Internal Server Error' };
 }
